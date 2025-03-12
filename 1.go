793270585d9185154c0dbbb1b9c662737878a911
Dauren <dauren@daurenmac.local>
2025-03-12 23:28:26 +0500

    why

--- 1.go
+++ 1.go
@@ -7,9 +7,14 @@ import "fmt"
 func isSubsequence(s string, t string) bool {
 	var i int
 	var j int
+
 	for i < len(s) && j < len(t) {
 		if s[i] == t[j] {
+			fmt.Println("oo", string(s[i]), string(t[j]), i, j)
 			i++
+
+		} else {
+			fmt.Println("nonono", string(t[j]), j)
 		}
 		j++
 		if i == len(s) {
@@ -22,10 +27,10 @@ func isSubsequence(s string, t string) bool {
 }
 
 func main() {
-	fmt.Println(isSubsequence("car", "cartwheel"))       // true
-	fmt.Println(isSubsequence("cwhl", "cartwheel"))      // true
-	fmt.Println(isSubsequence("cwheel", "cartwheel"))    // true
-	fmt.Println(isSubsequence("cartwheel", "cartwheel")) // true
-	fmt.Println(isSubsequence("cwheeel", "cartwheel"))   // false
-	fmt.Println(isSubsequence("lw", "cartwheel"))        // false
+	fmt.Println(isSubsequence("cra", "cartwheel")) // true
+	// fmt.Println(isSubsequence("cwhl", "cartwheel"))      // true
+	// fmt.Println(isSubsequence("cwheel", "cartwheel"))    // true
+	// fmt.Println(isSubsequence("cartwheel", "cartwheel")) // true
+	// fmt.Println(isSubsequence("cwheeel", "cartwheel"))   // false
+	// fmt.Println(isSubsequence("lw", "cartwheel"))        // false
 }
